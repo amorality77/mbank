692b8581ba0674555d72540363c7a12fd8a2028a
11th - Amorality <11th-amorality@MacBook-Air-11th.local>
2026-01-15 16:38:33 +0600

    Registration v2

--- src/kg_mbank/registration/Registration.java
+++ src/kg_mbank/registration/Registration.java
@@ -2,6 +2,7 @@ package kg_mbank.registration;
 
 public class Registration {
     public static void main(String[] args) {
-        System.out.println("Registration");
+        System.out.println("Registration v1");
+        System.out.println("Registration v2");
     }
 }
