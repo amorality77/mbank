8eae771cb810990d0f67bd3a1b15dce3480a7b60
11th - Amorality <11th-amorality@MacBook-Air-11th.local>
2026-01-15 19:57:32 +0600

    January promotions Andrei123
    january promotion1
    
    january promotion2
    
    january promotion3
    
    january promotion4
    
    january promotion5
    
    january promotion6
    
    january promotion7
    
    january promotion8

---
+++ src/kg_mbank/promotions/JanuaryPromotion.java
@@ -0,0 +1,15 @@
+package kg_mbank.promotions;
+
+public class JanuaryPromotion {
+    public static void main(String[] args) {
+        System.out.println("January ");
+        System.out.println("January123 ");
+        System.out.println("January1234 ");
+        System.out.println("January12345 ");
+        System.out.println("January123456 ");
+        System.out.println("January1234567 ");
+        System.out.println("January12345678 ");
+        System.out.println("January123456789 ");
+        System.out.println("January12345678910 ");
+    }
+}
