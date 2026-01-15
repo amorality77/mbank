d857956bc0da1e41c47f059384fe602d2c2360e3
11th - Amorality <11th-amorality@MacBook-Air-11th.local>
2026-01-15 17:18:46 +0600

    resolver conflict

---
+++ src/kg_mbank/delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg_mbank.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("Delivery yandex ");
+        System.out.println("Delivery glovo ");
+    }
+}
