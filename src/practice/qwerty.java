76621a949defc824d2b911f137aa034302362d49
11th-amorality <mirfasle228@mail.ru>
2026-01-21 23:20:24 +0600

    kjkl

--- src/practice/qwerty.java
+++ src/practice/qwerty.java
@@ -4,6 +4,6 @@ public class qwerty {
     public static void main(String[] args) {
         System.out.println("sxdcfvgbhnj");
         int a = 10;
-        int a = 10;
+        int b = 10;
     }
 }
